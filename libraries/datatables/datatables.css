e050648503ccddcd47b233ffdfac53b4b2ebdbd7
marcinzarycki <marcin.zarycki@kineticdata.com>
2016-06-27 09:19:59 -0500

    Updated datatables to new files which work with scriptpack
    The files look identical, but there's some kind of different encoding
    making the old ones error inside the scriptpack.

--- libraries/datatables/datatables.css
+++ libraries/datatables/datatables.css
@@ -2,8 +2,7 @@
  * This combined file was created by the DataTables downloader builder:
  *   https://datatables.net/download
  *
- * To rebuild or modify this file with the latest versions of the included
- * software please visit:
+ * To rebuild or modify this file with the latest versions of the included software please visit:
  *   https://datatables.net/download/#bs/jqc-1.12.0,jszip-2.5.0,pdfmake-0.1.18,dt-1.10.11,af-2.1.1,b-1.1.2,b-colvis-1.1.2,b-flash-1.1.2,b-html5-1.1.2,b-print-1.1.2,cr-1.3.1,fc-3.2.1,fh-3.1.1,kt-2.1.1,r-2.0.2,rr-1.1.1,sc-1.4.1,se-1.1.2
  *
  * Included libraries:
